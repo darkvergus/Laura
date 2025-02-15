1ab5259b3427e3cfec05f285f83490196ffaad7f
jacob <jakub.gordiak@gmail.com>
2025-02-15 06:39:14 +0100

    doc(GUID): Add useful comments

--- Laura/src/Core/GUID.cpp
+++ Laura/src/Core/GUID.cpp
@@ -5,6 +5,7 @@
 
 namespace Laura
 {
+	// global for this translation unit (static)
 	static std::random_device s_RandomDevice;
 	static std::mt19937_64 s_RandomEngine(s_RandomDevice());
 	static std::uniform_int_distribution<uint64_t> s_UniformDistribution;
--- Laura/src/Core/GUID.h
+++ Laura/src/Core/GUID.h
@@ -3,19 +3,23 @@
 
 namespace Laura
 {
+	// Globally Unique IDentifier
+	// A wrapper for a uint64_t
 	class GUID
 	{
 	public:
-		GUID();
+		GUID(); // constructs a random uint64_t GUID
 		GUID(uint64_t guid);
-		GUID(const GUID& other);
-		operator uint64_t() const { return m_GUID; }
+		GUID(const GUID& other); // copy constructor
+		operator uint64_t() const { return m_GUID; } // uint64_t conversion operator 
 		~GUID() = default;
 	private:
 		uint64_t m_GUID;
 	};
 }
 
+
+// Hashing specialization for GUIDs for use in std::unordered_map ...
 namespace std
 {
 	template<>
