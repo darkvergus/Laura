359829ea244dfc61ec1f977092e35ec2c5121c3e
jakubg05 <jakub.gordiak@gmail.com>
2025-07-11 11:13:18 +0200

    chore: log C++ standard version at startup

--- Laura/src/Core/application.cpp
+++ Laura/src/Core/application.cpp
@@ -5,6 +5,8 @@ namespace Laura
 	void Application::init()
 	{
 		Log::Init();
+		LR_CORE_INFO("C++ version: {0}", __cplusplus);
+		
 		_Profiler = std::make_shared<Profiler>(500);
 
 		_Window = IWindow::createWindow();
